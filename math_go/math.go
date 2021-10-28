1d136623511c67adfde581d8b366edbc88ba01b7
RandomEstimate <randometimate@sina.com>
2021-10-28 18:29:31 +0800

    debug

--- math_go/math.go
+++ math_go/math.go
@@ -16,40 +16,29 @@ const (
 
 // Max
 func MaxOrMin(slice interface{}, m Mode) interface{} {
-	var Type = []reflect.Kind{
-		reflect.Int,
-		reflect.Int8,
-		reflect.Int16,
-		reflect.Int32,
-		reflect.Int64,
-		reflect.Uint,
-		reflect.Uint8,
-		reflect.Uint16,
-		reflect.Uint32,
-		reflect.Uint64,
-		reflect.Uintptr,
-		reflect.Float32,
-		reflect.Float64,
-	}
 
 	if slice := reflect.ValueOf(slice); slice.Kind() == reflect.Slice {
 		if slice.Len() == 0 {
 			return nil
 		}
 
-		if max := reflect.ValueOf(slice.Index(0).Interface()); common.Index(max.Kind(), Type) == -1 {
-			return nil
-		}
-		slice2 := make([]float64, 0, slice.Len())
-		for i := 0; i < slice.Len(); i++ {
-			slice2 = append(slice2, common.ParamSlice(strconv.ParseFloat(fmt.Sprint(slice.Index(i).Interface()), 64))[0].(float64))
-		}
+		switch reflect.ValueOf(slice.Index(0).Interface()).Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint,
+			reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr, reflect.Float32, reflect.Float64:
 
-		switch m {
-		case MaxMode:
-			return (interface{})(max(slice2))
-		case MinMode:
-			return (interface{})(min(slice2))
+			slice2 := make([]float64, 0, slice.Len())
+			for i := 0; i < slice.Len(); i++ {
+				slice2 = append(slice2, common.ParamSlice(strconv.ParseFloat(fmt.Sprint(slice.Index(i).Interface()), 64))[0].(float64))
+			}
+
+			switch m {
+			case MaxMode:
+				return (interface{})(max(slice2))
+			case MinMode:
+				return (interface{})(min(slice2))
+			}
+		default:
+			return nil
 		}
 
 	}
